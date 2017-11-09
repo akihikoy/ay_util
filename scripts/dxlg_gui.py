6e5f6708136f3e49b424ecf596c5a681725e32dd
Akihiko Yamaguchi <info@akihikoy.net>
2017-11-09 16:36:47 +0900

    Added dxlg_gui.py:   GUI interface to control DxlGripper.
    Modified rqnb_gui.py:
      Joystick demo can be launched from the GUI (i.e. we do not have to execute ay_trick/scripts/cui_tool.py).

--- scripts/dxlg_gui.py
+++ scripts/dxlg_gui.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
-#\file    rqnb_gui.py
-#\brief   RobotiqNB GUI control panel.
+#\file    dxlg_gui.py
+#\brief   DxlGripper GUI control panel.
 #\author  Akihiko Yamaguchi, info@akihikoy.net
 #\version 0.1
-#\date    Sep.10, 2017
+#\date    Nov.08, 2017
 import roslib; roslib.load_manifest('ay_py')
 from ay_py.tool.py_gui import RunTerminalTab
 
@@ -11,34 +11,35 @@ if __name__=='__main__':
   E= 'Enter'
   terminals= [
     ('main',[
-      ('Init',(':all','ros',E,'robotiqnb',E)),
+      ('Init(USB0)',(':all','ros',E,'dxlgripper0',E)),
+      ('Init(USB1)',(':all','ros',E,'dxlgripper1',E)),
       ('Exit',':close') ]),
     ('roscore',[
       (':pair', ('roscore',['roscore',E]),
                 ('kill',['C-c']) )  ]),
-    ('RobotiqNB',[
-      (':pair', ('system(js0)',['roslaunch ay_util robotiq.launch jsdev:=/dev/input/js0',E]),
+    ('Joystick',[
+      (':pair', ('js0',['rosrun joy joy_node _dev:=/dev/input/js0',E]),
                 ('kill',['C-c']) ),
-      (':pair', ('system(js1)',['roslaunch ay_util robotiq.launch jsdev:=/dev/input/js1',E]),
+      (':pair', ('js1',['rosrun joy joy_node _dev:=/dev/input/js1',E]),
                 ('kill',['C-c']) )  ]),
-    ('Rq-Calib',[
-      ('calib',['rosrun ay_util rqnb_calib.py',E])  ]),
     ('Monitor-joy',[
       (':pair', ('echo-joy',['rostopic echo /joy',E]),
                 ('kill',['C-c']) )  ]),
+    ('Test_DxlGripper',[
+      ('test',['rosrun ay_py dxlg1.py',E])  ]),
     ('rviz',[
       (':pair', ('rviz',['rviz',E]),
                 ('kill',['C-c']) )  ]),
     ('time',[
       (':pair', ('time',['rosrun ay_vision disp_rostime',E]),
                 ('kill',['C-c']) )  ]),
-    ('aypi10',[
-      (':pair', ('stream',['ssh aypi10 "./stream.sh"',E]),
+    ('aypi11',[
+      (':pair', ('stream',['ssh aypi11 "./stream.sh"',E]),
                 ('stop',[E]) ),
-      ('reboot',['ssh aypi10 "sudo reboot"',E]),
-      ('shutdown',['ssh aypi10 "sudo halt -p"',E])  ]),
-    ('fv10',[
-      (':pair', ('start',['roslaunch ay_vision visual_skin_2fay10a2.launch',E]),
+      ('reboot',['ssh aypi11 "sudo reboot"',E]),
+      ('shutdown',['ssh aypi11 "sudo halt -p"',E])  ]),
+    ('fv11',[
+      (':pair', ('start',['roslaunch ay_vision visual_skin_2fay11a2.launch',E]),
                 ('kill',['C-c']) )  ]),
     ('JoyStickDemo',[
       (':pair', ('start',['roscd ay_trick',E,'rosrun ay_trick direct_run.py j',E]),
@@ -55,4 +56,4 @@ if __name__=='__main__':
                 #('kill',['C-c']) )  ]),
     ]
   exit_command= [E,'C-c']
-  RunTerminalTab('RobotiqNB Launcher',terminals,exit_command)
+  RunTerminalTab('DxlGripper Launcher',terminals,exit_command)
