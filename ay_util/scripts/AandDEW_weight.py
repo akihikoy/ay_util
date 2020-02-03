6d5ae911e4cb397a15587d97755822cd318d0c34
Akihiko Yamaguchi <info@akihikoy.net>
2020-02-03 20:03:26 +0900

    Added stamped basic-type messages:   Some of them are used in AandDEW_weight;

--- ay_util/scripts/AandDEW_weight.py
+++ ay_util/scripts/AandDEW_weight.py
@@ -4,12 +4,15 @@
 #\author  Akihiko Yamaguchi, info@akihikoy.net
 #\version 0.1
 #\date    Aug.26, 2019
-#TODO: It should be published with a time stamp.
+#\version 0.2
+#\date    Feb.03, 2020
+#         Modified to publish messages with with time stamps.
 
 import roslib;
-roslib.load_manifest('std_msgs')
+roslib.load_manifest('ay_util_msgs')
 import rospy
 import std_msgs.msg
+import ay_util_msgs.msg
 import sys
 import serial
 
@@ -20,18 +23,28 @@ if __name__=='__main__':
 
   ser= serial.Serial(dev,baudrate,serial.SEVENBITS,serial.PARITY_EVEN)
 
-  pub_value= rospy.Publisher('~value', std_msgs.msg.Float64, queue_size=1)
-  pub_raw= rospy.Publisher('~raw', std_msgs.msg.String, queue_size=1)
+  pub_value= rospy.Publisher('~value', ay_util_msgs.msg.Float64Stamped, queue_size=1)
+  pub_raw= rospy.Publisher('~raw', ay_util_msgs.msg.StringStamped, queue_size=1)
+
+  header= std_msgs.msg.Header()
+  header.frame_id= ''  #Shall we use node name? (rospy.get_name())
 
   try:
     while not rospy.is_shutdown():
       raw= ser.readline()
+      header.stamp= rospy.Time.now()
       print '"{raw}" ({l})'.format(raw=repr(raw), l=len(raw))
       if len(raw)!=17:  continue
       value= float(raw[3:12])
 
-      pub_value.publish(value)
-      pub_raw.publish(raw)
+      value_msg= ay_util_msgs.msg.Float64Stamped()
+      value_msg.header= header
+      value_msg.data= value
+      raw_msg= ay_util_msgs.msg.StringStamped()
+      raw_msg.header= header
+      raw_msg.data= raw
+      pub_value.publish(value_msg)
+      pub_raw.publish(raw_msg)
 
   finally:
     ser.close()
