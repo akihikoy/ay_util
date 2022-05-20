d6604168db1820783b1141799021016665740af5
Akihiko Yamaguchi <info@akihikoy.net>
2022-05-20 11:44:07 +0900

    Modified fix_usb_latency.sh to set arbitrary latency timer;

--- ay_util/scripts/fix_usb_latency.sh
+++ ay_util/scripts/fix_usb_latency.sh
@@ -4,9 +4,12 @@
 #\author  Akihiko Yamaguchi, info@akihikoy.net
 #\version 0.1
 #\date    Jun.01, 2018
+#\version 0.2
+#\date    May.20, 2022
+#         Modified to set arbitrary latency timer.
 
-#Usage(1): $ ./fix_usb_latency.sh ttyUSB0
-#Usage(2): $ rosrun ay_util fix_usb_latency.sh ttyUSB0
+#Usage(1): $ ./fix_usb_latency.sh [ttyUSB0 [LATENCY_TIMER]]
+#Usage(2): $ rosrun ay_util fix_usb_latency.sh [ttyUSB0 [LATENCY_TIMER]]
 #Usage(3):
 #  In order to let a normal user use this script without a root privelege,
 #  $ sudo ln -s `rospack find ay_util`/scripts/fix_usb_latency.sh /sbin/
@@ -18,10 +21,23 @@
 #  $ sudo /sbin/fix_usb_latency.sh ttyUSB0      #No password requested.
 
 dev=ttyUSB0
+latency_timer=1
 if [ $# -ge 1 ];then
   dev=$1
 fi
-if [ -f /sys/bus/usb-serial/devices/$dev/latency_timer ] && [ `cat /sys/bus/usb-serial/devices/$dev/latency_timer` -gt 1 ];then
-  echo "Fixing the latency issue of $dev..."
-  echo 1 | sudo tee /sys/bus/usb-serial/devices/$dev/latency_timer
+num_re='^[0-9]+$'
+if [ $# -ge 2 ] && [[ $2 =~ $num_re ]];then
+  latency_timer=$2
+fi
+config_file=/sys/bus/usb-serial/devices/$dev/latency_timer
+if [ -f $config_file ];then
+  echo "Config file: $config_file"
+  if [ `cat $config_file` -ne $latency_timer ];then
+    echo "Setting the latency timer of $dev to $latency_timer..."
+    echo "$latency_timer" | sudo tee $config_file
+  else
+    echo "The latency timer of $dev is already $latency_timer."
+  fi
+else
+  echo "Config file does not exist: $config_file"
 fi
