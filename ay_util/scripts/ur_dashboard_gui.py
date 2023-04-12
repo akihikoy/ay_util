9b32810d1021fab1a8f3046bffc0561462ecd11c
Akihiko Yamaguchi <info@akihikoy.net>
2023-04-12 15:55:04 +0900

    Separated the UR operation class from the dashboard;

--- ay_util/scripts/ur_dashboard_gui.py
+++ ay_util/scripts/ur_dashboard_gui.py
@@ -30,7 +30,177 @@ except Exception as e:
   print e
 from proc_manager import TSubProcManager
 
-class TProcessManagerUR(QtCore.QObject, TSubProcManager):
+#Simple module to operate UR.
+class TURManager(object):
+  def __init__(self, is_sim=False):
+    self.ur_robot_mode_names= {
+      ur_dashboard_msgs.msg.RobotMode.POWER_OFF: 'POWER_OFF',
+      ur_dashboard_msgs.msg.RobotMode.BOOTING: 'BOOTING',
+      ur_dashboard_msgs.msg.RobotMode.IDLE: 'IDLE',
+      ur_dashboard_msgs.msg.RobotMode.RUNNING: 'RUNNING' }
+    self.ur_safety_mode_names= {
+      ur_dashboard_msgs.msg.SafetyMode.NORMAL: 'NORMAL',
+      ur_dashboard_msgs.msg.SafetyMode.PROTECTIVE_STOP: 'PROTECTIVE_STOP',
+      ur_dashboard_msgs.msg.SafetyMode.ROBOT_EMERGENCY_STOP: 'ROBOT_EMERGENCY_STOP',
+      ur_dashboard_msgs.msg.SafetyMode.FAULT: 'FAULT' }
+
+    self.is_sim= is_sim
+
+    #self.panel= None
+    self.ur_ros_running= None
+    self.ur_topic_stamp= None
+    self.io_states= None
+    self.srvp_ur_dashboard= {}
+    self.srvp_ur_set_io= None
+    self.ur_robot_mode= None
+    self.ur_safety_mode= None
+    self.ur_program_running= None
+
+  def ConnectToURDashboard(self, timeout=6.0):
+    if not self.is_sim:
+      services= ['power_on', 'power_off', 'brake_release', 'play', 'stop', 'shutdown', 'unlock_protective_stop', 'restart_safety', 'close_safety_popup']
+      #for service in services:
+        #self.srvp_ur_dashboard[service]= SetupServiceProxy('/ur_hardware_interface/dashboard/{0}'.format(service), std_srvs.srv.Trigger, persistent=False, time_out=3.0)
+      #self.srvp_ur_set_io= SetupServiceProxy('/ur_hardware_interface/set_io', ur_msgs.srv.SetIO, persistent=False, time_out=10.0)
+      threads= {}
+      for service in services:
+        threads[service]= threading.Thread(name=service, target=lambda service=service:(self.srvp_ur_dashboard.__setitem__(service,SetupServiceProxy('/ur_hardware_interface/dashboard/{0}'.format(service), std_srvs.srv.Trigger, persistent=False, time_out=timeout))))
+        threads[service].start()
+      threads['srvp_ur_set_io']= threading.Thread(name='srvp_ur_set_io', target=lambda:(setattr(self,'srvp_ur_set_io',SetupServiceProxy('/ur_hardware_interface/set_io', ur_msgs.srv.SetIO, persistent=False, time_out=timeout))))
+      threads['srvp_ur_set_io'].start()
+      for name,th in threads.iteritems():  th.join()
+
+    #Connect to io_states to publish a fake io_states.
+    self.pub_io_states= rospy.Publisher('/ur_hardware_interface/io_states', ur_msgs.msg.IOStates, queue_size=10)
+
+    self.sub_joint_states= rospy.Subscriber('/joint_states', sensor_msgs.msg.JointState, self.JointStatesCallback)
+    self.sub_io_states= rospy.Subscriber('/ur_hardware_interface/io_states', ur_msgs.msg.IOStates, self.IOStatesCallback)
+
+    if not self.is_sim:
+      self.sub_robot_mode= rospy.Subscriber('/ur_hardware_interface/robot_mode', ur_dashboard_msgs.msg.RobotMode, self.RobotModeCallback)
+      self.sub_safety_mode= rospy.Subscriber('/ur_hardware_interface/safety_mode', ur_dashboard_msgs.msg.SafetyMode, self.SafetyModeCallback)
+      self.sub_program_running= rospy.Subscriber('/ur_hardware_interface/robot_program_running', std_msgs.msg.Bool, self.ProgramRunningCallback)
+
+  def DisconnectUR(self):
+    if self.is_sim:  return
+    self.srvp_ur_set_io= None  #TODO: srvp_ur_set_io may be used from other thread, so the LED may be turned on before this.
+    self.srvp_ur_dashboard= {}
+
+    self.sub_joint_states.unregister()
+    self.sub_joint_states= None
+    if not self.is_sim:
+      self.sub_robot_mode.unregister()
+      self.sub_safety_mode.unregister()
+      self.sub_program_running.unregister()
+      self.sub_robot_mode= None
+      self.sub_safety_mode= None
+      self.sub_program_running= None
+
+    self.pub_io_states.unregister()
+    self.pub_io_states= None
+
+  def RunURDashboard(self, service):
+    if service not in self.srvp_ur_dashboard:
+      return False
+    res= self.srvp_ur_dashboard[service]()
+    return res.success
+
+  #int8 fun, int8 pin, float32 state
+  #fun: ur_msgs.srv.SetIORequest.{FUN_SET_DIGITAL_OUT,FUN_SET_FLAG,FUN_SET_ANALOG_OUT,FUN_SET_TOOL_VOLTAGE}
+  #state: ur_msgs.srv.SetIORequest.{STATE_OFF,STATE_ON}
+  def SetURIO(self, fun, pin, state):
+    if self.srvp_ur_set_io is None:  return
+    return self.srvp_ur_set_io(ur_msgs.srv.SetIORequest(fun, pin, state)).success
+
+  def JointStatesCallback(self, msg):
+    self.ur_topic_stamp= rospy.Time.now()
+
+  def IOStatesCallback(self, msg):
+    self.io_states= msg
+
+  def RobotModeCallback(self, msg):
+    self.ur_robot_mode= msg.mode
+
+  def SafetyModeCallback(self, msg):
+    self.ur_safety_mode= msg.mode
+    if self.ur_safety_mode!=ur_dashboard_msgs.msg.SafetyMode.NORMAL and self.ur_program_running:
+      self.RunURDashboard('stop')
+      self.WaitForProgramRunning(False, timeout=1.0)
+
+  def ProgramRunningCallback(self, msg):
+    self.ur_program_running= msg.data
+
+  #mode: ur_dashboard_msgs.msg.RobotMode.{POWER_OFF,BOOTING,IDLE,RUNNING}
+  def WaitForRobotMode(self, mode, timeout=20):
+    if self.is_sim:  return True
+    if mode==ur_dashboard_msgs.msg.RobotMode.POWER_OFF and\
+      self.ur_robot_mode in (ur_dashboard_msgs.msg.RobotMode.BOOTING,
+                             ur_dashboard_msgs.msg.RobotMode.IDLE,
+                             ur_dashboard_msgs.msg.RobotMode.RUNNING):  return False
+    if mode==ur_dashboard_msgs.msg.RobotMode.IDLE and\
+      self.ur_robot_mode in (ur_dashboard_msgs.msg.RobotMode.RUNNING,):  return False
+    t_start= rospy.Time.now()
+    rate= rospy.Rate(20)
+    #print 'WaitForRobotMode',mode,self.ur_robot_mode
+    while self.ur_robot_mode != mode:
+      rate.sleep()
+      if (rospy.Time.now()-t_start).to_sec()>timeout:
+        print 'WaitForRobotMode timeout.'
+        return False
+    #print '  done.',mode,self.ur_robot_mode
+    return True
+
+  #mode: ur_dashboard_msgs.msg.SafetyMode.{NORMAL,PROTECTIVE_STOP,ROBOT_EMERGENCY_STOP,FAULT}
+  def WaitForSafetyMode(self, mode, timeout=20):
+    if self.is_sim:  return True
+    t_start= rospy.Time.now()
+    rate= rospy.Rate(20)
+    while self.ur_safety_mode != mode:
+      rate.sleep()
+      if (rospy.Time.now()-t_start).to_sec()>timeout:
+        print 'WaitForSafetyMode timeout.'
+        return False
+    return True
+
+  #program_running: True or False
+  def WaitForProgramRunning(self, program_running, timeout=20):
+    if self.is_sim:  return True
+    t_start= rospy.Time.now()
+    rate= rospy.Rate(20)
+    while self.ur_program_running != program_running:
+      rate.sleep()
+      if (rospy.Time.now()-t_start).to_sec()>timeout:
+        print 'WaitForProgramRunning timeout.'
+        return False
+    return True
+
+  #ur_ros_running: True or False
+  def WaitForURROSRunning(self, ur_ros_running, timeout=20):
+    if self.is_sim:  return True
+    t_start= rospy.Time.now()
+    rate= rospy.Rate(20)
+    while self.ur_ros_running != ur_ros_running:
+      rate.sleep()
+      if (rospy.Time.now()-t_start).to_sec()>timeout:
+        print 'WaitForURROSRunning timeout.'
+        return False
+    return True
+
+  def SendFakeDigitalInSignal(self, signal_idx, signal_trg):
+    if self.ur_robot_mode is not None and self.io_states is not None:
+      msg= copy.deepcopy(self.io_states)
+    else:
+      msg= ur_msgs.msg.IOStates()
+      msg.digital_in_states= [ur_msgs.msg.Digital(pin,False) for pin in range(18)]
+      msg.digital_out_states= [ur_msgs.msg.Digital(pin,False) for pin in range(18)]
+      msg.flag_states= [ur_msgs.msg.Digital(pin,False) for pin in range(2)]
+      msg.analog_in_states= [ur_msgs.msg.Analog(pin,0,0) for pin in range(2)]
+      msg.analog_out_states= [ur_msgs.msg.Analog(pin,0,0) for pin in range(2)]
+    msg.digital_in_states[signal_idx]= ur_msgs.msg.Digital(signal_idx,signal_trg)
+    self.pub_io_states.publish(msg)
+
+
+class TProcessManagerUR(QtCore.QObject, TSubProcManager, TURManager):
 
   #Definition of states:
   UNDEFINED= -10
@@ -148,6 +318,7 @@ class TProcessManagerUR(QtCore.QObject, TSubProcManager):
     self.config= config
     QtCore.QObject.__init__(self)
     TSubProcManager.__init__(self)
+    TURManager.__init__(self, is_sim)
     self.node_name= node_name
     self.is_sim= is_sim
 
@@ -166,16 +337,6 @@ class TProcessManagerUR(QtCore.QObject, TSubProcManager):
       self.WAIT_REQUEST:        'WAIT_REQUEST'        ,
       self.PROGRAM_RUNNING:     'PROGRAM_RUNNING'     ,
       }
-    self.ur_robot_mode_names= {
-      ur_dashboard_msgs.msg.RobotMode.POWER_OFF: 'POWER_OFF',
-      ur_dashboard_msgs.msg.RobotMode.BOOTING: 'BOOTING',
-      ur_dashboard_msgs.msg.RobotMode.IDLE: 'IDLE',
-      ur_dashboard_msgs.msg.RobotMode.RUNNING: 'RUNNING' }
-    self.ur_safety_mode_names= {
-      ur_dashboard_msgs.msg.SafetyMode.NORMAL: 'NORMAL',
-      ur_dashboard_msgs.msg.SafetyMode.PROTECTIVE_STOP: 'PROTECTIVE_STOP',
-      ur_dashboard_msgs.msg.SafetyMode.ROBOT_EMERGENCY_STOP: 'ROBOT_EMERGENCY_STOP',
-      ur_dashboard_msgs.msg.SafetyMode.FAULT: 'FAULT' }
     self.script_node_status_names= {
       ay_trick_msgs.msg.ROSNodeMode.BUSY: 'BUSY',
       ay_trick_msgs.msg.ROSNodeMode.READY: 'READY',
@@ -184,15 +345,7 @@ class TProcessManagerUR(QtCore.QObject, TSubProcManager):
     #self.panel= None
     self.status= self.UNDEFINED
     self.status_color= []  #List of 'red','yellow','green'
-    self.ur_ros_running= None
     self.script_node_running= None
-    self.ur_topic_stamp= None
-    self.io_states= None
-    self.srvp_ur_dashboard= {}
-    self.srvp_ur_set_io= None
-    self.ur_robot_mode= None
-    self.ur_safety_mode= None
-    self.ur_program_running= None
     self.script_node_status= None
     self.script_node_status_stamp= None
 
@@ -202,63 +355,6 @@ class TProcessManagerUR(QtCore.QObject, TSubProcManager):
   def InitNode(self):
     rospy.init_node(self.node_name)
     rospy.sleep(0.1)
-
-  def ConnectToURDashboard(self, timeout=6.0):
-    if not self.is_sim:
-      services= ['power_on', 'power_off', 'brake_release', 'play', 'stop', 'shutdown', 'unlock_protective_stop', 'restart_safety', 'close_safety_popup']
-      #for service in services:
-        #self.srvp_ur_dashboard[service]= SetupServiceProxy('/ur_hardware_interface/dashboard/{0}'.format(service), std_srvs.srv.Trigger, persistent=False, time_out=3.0)
-      #self.srvp_ur_set_io= SetupServiceProxy('/ur_hardware_interface/set_io', ur_msgs.srv.SetIO, persistent=False, time_out=10.0)
-      threads= {}
-      for service in services:
-        threads[service]= threading.Thread(name=service, target=lambda service=service:(self.srvp_ur_dashboard.__setitem__(service,SetupServiceProxy('/ur_hardware_interface/dashboard/{0}'.format(service), std_srvs.srv.Trigger, persistent=False, time_out=timeout))))
-        threads[service].start()
-      threads['srvp_ur_set_io']= threading.Thread(name='srvp_ur_set_io', target=lambda:(setattr(self,'srvp_ur_set_io',SetupServiceProxy('/ur_hardware_interface/set_io', ur_msgs.srv.SetIO, persistent=False, time_out=timeout))))
-      threads['srvp_ur_set_io'].start()
-      for name,th in threads.iteritems():  th.join()
-
-    #Connect to io_states to publish a fake io_states.
-    self.pub_io_states= rospy.Publisher('/ur_hardware_interface/io_states', ur_msgs.msg.IOStates, queue_size=10)
-
-    self.sub_joint_states= rospy.Subscriber('/joint_states', sensor_msgs.msg.JointState, self.JointStatesCallback)
-    self.sub_io_states= rospy.Subscriber('/ur_hardware_interface/io_states', ur_msgs.msg.IOStates, self.IOStatesCallback)
-
-    if not self.is_sim:
-      self.sub_robot_mode= rospy.Subscriber('/ur_hardware_interface/robot_mode', ur_dashboard_msgs.msg.RobotMode, self.RobotModeCallback)
-      self.sub_safety_mode= rospy.Subscriber('/ur_hardware_interface/safety_mode', ur_dashboard_msgs.msg.SafetyMode, self.SafetyModeCallback)
-      self.sub_program_running= rospy.Subscriber('/ur_hardware_interface/robot_program_running', std_msgs.msg.Bool, self.ProgramRunningCallback)
-
-  def DisconnectUR(self):
-    if self.is_sim:  return
-    self.TurnOffLEDAll()
-    self.srvp_ur_set_io= None  #TODO: srvp_ur_set_io may be used from other thread, so the LED may be turned on before this.
-    self.srvp_ur_dashboard= {}
-
-    self.sub_joint_states.unregister()
-    self.sub_joint_states= None
-    if not self.is_sim:
-      self.sub_robot_mode.unregister()
-      self.sub_safety_mode.unregister()
-      self.sub_program_running.unregister()
-      self.sub_robot_mode= None
-      self.sub_safety_mode= None
-      self.sub_program_running= None
-
-    self.pub_io_states.unregister()
-    self.pub_io_states= None
-
-  def RunURDashboard(self, service):
-    if service not in self.srvp_ur_dashboard:
-      return False
-    res= self.srvp_ur_dashboard[service]()
-    return res.success
-
-  #int8 fun, int8 pin, float32 state
-  #fun: ur_msgs.srv.SetIORequest.{FUN_SET_DIGITAL_OUT,FUN_SET_FLAG,FUN_SET_ANALOG_OUT,FUN_SET_TOOL_VOLTAGE}
-  #state: ur_msgs.srv.SetIORequest.{STATE_OFF,STATE_ON}
-  def SetURIO(self, fun, pin, state):
-    if self.srvp_ur_set_io is None:  return
-    return self.srvp_ur_set_io(ur_msgs.srv.SetIORequest(fun, pin, state)).success
 
   def TurnOffLEDAll(self):
     self.SetLEDLight('red', False)
@@ -296,93 +392,10 @@ class TProcessManagerUR(QtCore.QObject, TSubProcManager):
       stop_state= ur_msgs.srv.SetIORequest.STATE_ON if is_stop_on else ur_msgs.srv.SetIORequest.STATE_OFF
       self.SetURIO(ur_msgs.srv.SetIORequest.FUN_SET_DIGITAL_OUT, stop_pin, stop_state)
 
-  def JointStatesCallback(self, msg):
-    self.ur_topic_stamp= rospy.Time.now()
-
-  def IOStatesCallback(self, msg):
-    self.io_states= msg
-
-  def RobotModeCallback(self, msg):
-    self.ur_robot_mode= msg.mode
-
-  def SafetyModeCallback(self, msg):
-    self.ur_safety_mode= msg.mode
-    if self.ur_safety_mode!=ur_dashboard_msgs.msg.SafetyMode.NORMAL and self.ur_program_running:
-      self.RunURDashboard('stop')
-      self.WaitForProgramRunning(False, timeout=1.0)
-
-  def ProgramRunningCallback(self, msg):
-    self.ur_program_running= msg.data
-
-  #mode: ur_dashboard_msgs.msg.RobotMode.{POWER_OFF,BOOTING,IDLE,RUNNING}
-  def WaitForRobotMode(self, mode, timeout=20):
-    if self.is_sim:  return True
-    if mode==ur_dashboard_msgs.msg.RobotMode.POWER_OFF and\
-      self.ur_robot_mode in (ur_dashboard_msgs.msg.RobotMode.BOOTING,
-                             ur_dashboard_msgs.msg.RobotMode.IDLE,
-                             ur_dashboard_msgs.msg.RobotMode.RUNNING):  return False
-    if mode==ur_dashboard_msgs.msg.RobotMode.IDLE and\
-      self.ur_robot_mode in (ur_dashboard_msgs.msg.RobotMode.RUNNING,):  return False
-    t_start= rospy.Time.now()
-    rate= rospy.Rate(20)
-    #print 'WaitForRobotMode',mode,self.ur_robot_mode
-    while self.ur_robot_mode != mode:
-      rate.sleep()
-      if (rospy.Time.now()-t_start).to_sec()>timeout:
-        print 'WaitForRobotMode timeout.'
-        return False
-    #print '  done.',mode,self.ur_robot_mode
-    return True
-
-  #mode: ur_dashboard_msgs.msg.SafetyMode.{NORMAL,PROTECTIVE_STOP,ROBOT_EMERGENCY_STOP,FAULT}
-  def WaitForSafetyMode(self, mode, timeout=20):
-    if self.is_sim:  return True
-    t_start= rospy.Time.now()
-    rate= rospy.Rate(20)
-    while self.ur_safety_mode != mode:
-      rate.sleep()
-      if (rospy.Time.now()-t_start).to_sec()>timeout:
-        print 'WaitForSafetyMode timeout.'
-        return False
-    return True
-
-  #program_running: True or False
-  def WaitForProgramRunning(self, program_running, timeout=20):
-    if self.is_sim:  return True
-    t_start= rospy.Time.now()
-    rate= rospy.Rate(20)
-    while self.ur_program_running != program_running:
-      rate.sleep()
-      if (rospy.Time.now()-t_start).to_sec()>timeout:
-        print 'WaitForProgramRunning timeout.'
-        return False
-    return True
-
-  #ur_ros_running: True or False
-  def WaitForURROSRunning(self, ur_ros_running, timeout=20):
-    if self.is_sim:  return True
-    t_start= rospy.Time.now()
-    rate= rospy.Rate(20)
-    while self.ur_ros_running != ur_ros_running:
-      rate.sleep()
-      if (rospy.Time.now()-t_start).to_sec()>timeout:
-        print 'WaitForURROSRunning timeout.'
-        return False
-    return True
-
-  def SendFakeDigitalInSignal(self, signal_idx, signal_trg):
-    if self.ur_robot_mode is not None and self.io_states is not None:
-      msg= copy.deepcopy(self.io_states)
-    else:
-      msg= ur_msgs.msg.IOStates()
-      msg.digital_in_states= [ur_msgs.msg.Digital(pin,False) for pin in range(18)]
-      msg.digital_out_states= [ur_msgs.msg.Digital(pin,False) for pin in range(18)]
-      msg.flag_states= [ur_msgs.msg.Digital(pin,False) for pin in range(2)]
-      msg.analog_in_states= [ur_msgs.msg.Analog(pin,0,0) for pin in range(2)]
-      msg.analog_out_states= [ur_msgs.msg.Analog(pin,0,0) for pin in range(2)]
-    msg.digital_in_states[signal_idx]= ur_msgs.msg.Digital(signal_idx,signal_trg)
-    self.pub_io_states.publish(msg)
-
+  def DisconnectUR(self):
+    if self.is_sim:  return
+    self.TurnOffLEDAll()
+    TURManager.DisconnectUR(self)
 
   def ConnectToScriptNode(self, timeout=20):
     self.connected_to_script_node= False
